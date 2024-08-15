f0f52e867e5153e2d214839d8a413f3250b84718
rosenvladimirov <vladimirov.rosen@gmail.com>
2024-08-15 16:29:52 +0300

    Refactor translation logic and remove `res_users` model
    Simplified the translation logic by introducing a common list of transliterate fields and loop. Removed `partner_multilang/models/res_users.py` and moved relevant functionality to `res_country.py`. Enhanced `write` method to handle multilanguage updates more effectively.

--- partner_multilang/models/__init__.py
+++ partner_multilang/models/__init__.py
@@ -4,4 +4,4 @@ from . import res_transliterate
 from . import res_lang
 from . import res_partner
 from . import res_company
-from . import res_users
+from . import res_country
--- partner_multilang/models/res_transliterate.py
+++ partner_multilang/models/res_transliterate.py
@@ -7,7 +7,9 @@ from odoo import api, models
 from polyglot.text import Text
 
 _logger = logging.getLogger(__name__)
-
+TRANSLITERATE_FIELDS = ['name', 'company_name',
+                        'city', 'street', 'street2',
+                        'private_city', 'private_street', 'private_street2']
 
 def partner_name_translate(name, lang, transliterate):
     if lang != "en_US" and transliterate:
@@ -35,7 +37,9 @@ class ResTransliterate(models.AbstractModel):
         result = super(ResTransliterate, self).get_view(
             view_id=view_id, view_type=view_type, **options
         )
-        if view_type in ["tree", "kanban"]:
+        value_fields = self.fields_get()
+        if value_fields.get('name') and value_fields['name']['type'] == 'char' \
+                and not value_fields['name'].get('related') and view_type in ["tree", "kanban"]:
             doc = etree.XML(result["arch"])
             for type_in in ["tree", "kanban"]:
                 for node in doc.xpath(f"//{type_in}"):
@@ -63,42 +67,30 @@ class ResTransliterate(models.AbstractModel):
 
     @api.depends_context('lang')
     def _force_multilanguage(self, vals, new_record=False):
-        if vals.get("city") and new_record:
-            lang, current_lang, transliterate = self._check_lang(vals["city"])
-            if lang != "en_US":
-                if current_lang != lang:
-                    record = self.with_context(**dict(self._context, lang=lang))
-                    record.city = vals['city']
-                record = self.with_context(**dict(self._context, lang="en_US"))
-                record.city = partner_name_translate(vals["city"], lang, transliterate)
-        if vals.get("street") and new_record:
-            lang, current_lang, transliterate = self._check_lang(vals["street"])
-            if lang != "en_US":
-                if current_lang != lang:
-                    record = self.with_context(**dict(self._context, lang=lang))
-                    record.street = vals["street"]
-                record = self.with_context(**dict(self._context, lang="en_US"))
-                record.street = partner_name_translate(vals["street"], lang, transliterate)
-        if vals.get("name") and new_record:
-            lang, current_lang, transliterate = self._check_lang(vals["name"])
-            if lang != "en_US":
-                if current_lang != lang:
-                    record = self.with_context(**dict(self._context, lang=lang))
-                    record.name = vals["name"]
-                record = self.with_context(**dict(self._context, lang="en_US"))
-                record.name = partner_name_translate(vals["name"], lang, transliterate)
-        if vals.get("company_name") and new_record:
-            lang, current_lang, transliterate = self._check_lang(vals["company_name"])
-            if lang != "en_US":
-                if current_lang != lang:
-                    record = self.with_context(**dict(self._context, lang=lang))
-                    record.company_name = vals["company_name"]
-                record = self.with_context(**dict(self._context, lang="en_US"))
-                record.company_name = partner_name_translate(vals["company_name"], lang, transliterate)
+        for field_name in TRANSLITERATE_FIELDS:
+            if vals.get(field_name) and new_record:
+                lang, current_lang, transliterate = self._check_lang(vals[field_name])
+                if lang != "en_US":
+                    if current_lang != lang:
+                        record = self.with_context(**dict(self._context, lang=lang, update_lang=True))
+                        record.write({
+                          field_name: vals[field_name],
+                        })
+                    record = self.with_context(**dict(self._context, lang="en_US", update_lang=True))
+                    record.write({
+                        field_name: partner_name_translate(vals[field_name], lang, transliterate)
+                    })
 
     @api.model_create_multi
     def create(self, vals_list):
         res = super().create(vals_list)
         for record, vals in zip(res, vals_list):
             record._force_multilanguage(vals, new_record=True)
+        return res
+
+    def write(self, vals):
+        res = super().write(vals)
+        if not self._context.get('update_lang', False):
+            for record in self:
+                record._force_multilanguage(vals, True)
         return res
