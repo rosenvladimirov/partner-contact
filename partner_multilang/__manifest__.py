0ec3fb94b171de84732ee892d809098f90e67456
rosenvladimirov <vladimirov.rosen@gmail.com>
2025-03-02 12:47:31 +0200

    Add multilanguage suppprt

--- partner_multilang/__manifest__.py
+++ partner_multilang/__manifest__.py
@@ -3,15 +3,15 @@
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "category": "Localization",
-    "complexity": "normal",
     "author": "Rosen Vladimirov,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "description": """
-        Multilang partner names.
+        Multilang partner names. Transliterate names.
     """,
     "external_dependencies": {
         "python": [
             "cyrtranslit",
+            "langdetect",
         ]
     },
     "depends": [
